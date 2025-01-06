49df017888f779b5a0b41403663cd9a1535be0e8
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-01-06 18:35:18 +0530

    first commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,93 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/home/Home";
+import Menu from "../pages/menuPage/Menu";
+import Signup from "../components/Signup";
+import Order from "../pages/dashboard/Order";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import UserProfile from "../pages/dashboard/UserProfile";
+import CartPage from "../pages/menuPage/CartPage";
+import Login from "../components/Login";
+import Dashboard from "../pages/dashboard/admin/Dashboard"
+import DashboardLayout from "../layout/DashboardLayout"
+import Users from "../pages/dashboard/admin/Users"
+import AddMenu from "../pages/dashboard/admin/AddMenu";
+import ManageItems from "../pages/dashboard/admin/ManageItems";
+import UpdateMenu from "../pages/dashboard/admin/UpdateMenu";
+import Payment from "../pages/menuPage/Payment";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/menu",
+        element: <Menu />
+      },
+      {
+        path: "/order",
+        element: <PrivateRoute><Order /></PrivateRoute>
+      },
+      {
+        path: "/update-profile",
+        element: <UserProfile />
+      },
+      {
+        path: "/cart-page",
+        element: <CartPage />
+      },
+      {
+        path: "/process-checkout",
+        element: <Payment />
+      }
+    ]
+  },
+  {
+    path: "/signup",
+    element: <Signup />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "dashboard",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout />
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        path: "",
+        element: <Dashboard />,
+      },
+      {
+        path: "users",
+        element: <Users />,
+      },
+      {
+        path: "add-menu",
+        element: <AddMenu />,
+      },
+      {
+        path: "manage-items",
+        element: <ManageItems />,
+      },
+      {
+        path: "update-menu/:id",
+        element: <UpdateMenu />,
+        loader: ({ params }) =>
+          fetch(`https://backend-for-food-app.onrender.com/menu/${params.id}`),
+      },
+
+    ]
+  }
+]);
+
+export default router;
